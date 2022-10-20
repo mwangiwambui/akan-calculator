d34cb8164380ca3c4d3b4349f5386c3404d1ff96
Naomi Wambui Mwangi <nwmwangi@safaricom.co.ke>
2022-10-20 19:49:48 +0300

    Added the modal for the akan name

--- script.js
+++ script.js
@@ -58,16 +58,45 @@ function calculateAkan() {
 }
 
 function getAkanNameAndCalculate() {
+    document.getElementById("validOutput").innerHTML = '';
     gender = document.getElementById('gender').value;
     const dateInput = document.getElementById('birthdayInput').value;
     if (validateDate(dateInput)) {
         akanIndex = parseInt(calculateAkan());
         console.log(akanIndex);
         getAkanName(akanIndex, gender);
-        document.getElementById("output").innerHTML = akanName
+        modal(akanName)
     } else {
-        document.getElementById("output").innerHTML = 'Enter a valid date';
+        document.getElementById("validOutput").innerHTML = 'Enter a valid date';
     }
+}
+
+function modal(akanName){
+    // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+document.getElementById("output").innerHTML = akanName
+// When the user clicks on the button, open the modal
+
+modal.style.display = "block";
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 }
 
 
